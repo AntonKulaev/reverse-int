aec30e99f310bc75787997d3308057de2efce6ea
KulaevAnton <kulaev-antyan@mail.ru>
2022-02-11 16:43:20 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let string = String(n);
+    let i = 0;
+    result = '';
+    if (string[0] === '-') {
+      string = string.substring(1);
+    } else {
+      string = String(n);
+    }
+    while (i < string.length) {
+      result = `${string[i]}${result}`;
+      i += 1;
+    }
+    return Number(result);
 }
+
